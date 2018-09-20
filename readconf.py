5ee296f96cc1a607d7dd4ec1fbc4186ef836c292
KLA <smtwkla@gmail.com>
2018-09-20 12:44:09 +0530

    remove readconf

--- readconf.py
+++
@@ -1,9 +0,0 @@
-
-
-
-if __name__ == "__main__":
-    c = readconfig()
-    con = f"{c['db']['dbdialect']}://{c['db']['user']}:{c['db']['pass']}@{c['db']['host']}:{c['db']['port']}/{c['db']['database']}"
-
-    print(con)
-
