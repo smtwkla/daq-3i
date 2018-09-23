ee530cdf7007d72a3f928aa29cc34f61c4701fbe
kla <smtwkla@gmail.com>
2018-09-23 09:04:35 +0530

    using BinaryPayloadDecoder from pymodbus to decode received data

--- buscommon.py
+++ buscommon.py
@@ -9,9 +9,11 @@ class ReadResponse:
         self.response = None
         self.exception = None
 
-def getSignedNumber(number, bitLength):
-    mask = (2 ** bitLength) - 1
+def get_signed_number(number, bitLength):
+    mask = (2 ** bitLength) - 1 # All Bits to 1 of bit length
     if number & (1 << (bitLength - 1)):
+        # Sign Bit is Set
         return number | ~mask
     else:
+        # Unsigned
         return number & mask
--- modbus.py
+++ modbus.py
@@ -1,7 +1,10 @@
 import logging
+from pymodbus.constants import Endian
+from pymodbus.payload import BinaryPayloadDecoder
 from pymodbus.client.sync import ModbusTcpClient
 import pymodbus.exceptions
 import buscommon
+
 
 MODBUS_FUNC_READHOLDING = 3
 
@@ -18,7 +21,7 @@ MODBUS_FLOAT_RBYTES = 9
 MODBUS_FLOAT_RWORDS = 10
 MODBUS_FLOAT_RSKIP2 = 11
 
-FORMAT_LENGTH = {MODBUS_SINT16: 1, MODBUS_SINT32: 1, MODBUS_SINT32_RWORDS: 2, MODBUS_SKIP2: 3,
+FORMAT_LENGTH = {MODBUS_SINT16: 1, MODBUS_SINT32: 2, MODBUS_SINT32_RWORDS: 2, MODBUS_SKIP2: 3,
                  MODBUS_UINT16: 4, MODBUS_UINT32: 5, MODBUS_UINT32_RWORDS: 6, MODBUS_FLOAT: 7,
                  MODBUS_FLOAT_SKIP2: 8, MODBUS_FLOAT_RBYTES: 9, MODBUS_FLOAT_RWORDS: 10,
                  MODBUS_FLOAT_RSKIP2: 11}
@@ -53,11 +56,54 @@ class ModbusMixin:
     def decode_data_format(self, chl, res):
 
         value = None
+        byteorder = Endian.Little
+        wordorder = Endian.Little
+
+        if chl.format == MODBUS_SINT32_RWORDS:
+            byteorder = Endian.Little
+            wordorder = Endian.Big
+
+        decoder = BinaryPayloadDecoder.fromRegisters(res.response.registers, byteorder=byteorder, wordorder=wordorder)
+
 
         if chl.format == MODBUS_UINT16:
-            value = res.response.registers[0]
+            value = decoder.decode_16bit_uint()
 
         if chl.format == MODBUS_SINT16:
-            value = buscommon.getSignedNumber(res.response.registers[0], 16)
+            value = decoder.decode_16bit_int()
 
+        if chl.format == MODBUS_SINT32:
+            value = decoder.decode_32bit_int()
+
+        if chl.format == MODBUS_UINT32:
+            value = decoder.decode_32bit_uint()
+
+        if chl.format == MODBUS_SINT32_RWORDS:
+            value = decoder.decode_32bit_int()
+
+        if chl.format == MODBUS_SKIP2:
+            pass
+
+        if chl.format == MODBUS_UINT32_RWORDS:
+            pass
+
+        if chl.format == MODBUS_FLOAT:
+            value = decoder.decode_32bit_float()
+
+        if chl.format == MODBUS_FLOAT_SKIP2:
+            pass
+
+        if chl.format == MODBUS_FLOAT_RBYTES:
+            pass
+
+        if chl.format == MODBUS_FLOAT_RWORDS:
+            pass
+
+        if chl.format == MODBUS_FLOAT_RSKIP2:
+            pass
+
+        # decoder.decode_bits()
         return value
+
+
+
