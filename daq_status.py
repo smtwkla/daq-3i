06a0896dd423b1178ffa6cf2ad49cc55e76bc715
kla <smtwkla@gmail.com>
2018-09-27 13:53:26 +0530

    working daq_status class

--- daq_status.py
+++ daq_status.py
@@ -1,3 +1,11 @@
+import logging
+from sqlalchemy.exc import SQLAlchemyError
+import db_model as db
+import datetime
+
+STATUS_RUNNING = 1
+STATUS_OK = 1
+
 """
 class DaqStatus - Manage Daq Status table
 """
@@ -5,9 +13,58 @@ class DaqStatus - Manage Daq Status table
 
 class DaqStatus:
 
-    def flush_parameters(self):
-        pass
+    def __init__(self, env):
+        self.env = env
 
-    def update_parameter(self):
-        pass
+    def flush_parameters(self):
+        #  Get new session
+        session = self.env.Session()
+
+        try:
+            #  Flush all old parameters
+            session.query(db.Daq_Status).delete()
+            #  Commit
+            session.commit()
+            #  Close
+            session.close()
+
+        except SQLAlchemyError as e:
+            session.rollback()
+            logging.critical("Error: {0}".format(e))
+
+
+    def update_parameter(self, parameter, status):
+
+        #  Get new session
+        session = self.env.Session()
+
+        try:
+
+            # Get parameter object
+            record = session.query(db.Daq_Status)\
+                .filter(db.Daq_Status.parameter == parameter)\
+                .first()
+
+            if record is None:
+                # update value
+                record = db.Daq_Status()
+                record.parameter = parameter
+                record.status = status
+                record.ts = datetime.datetime.now()
+                session.add(record)
+            else:
+                record.status = status
+                record.ts = datetime.datetime.now()
+
+            #  Commit
+            session.commit()
+            #  Close
+            session.close()
+            return False
+
+        except SQLAlchemyError as e:
+            session.rollback()
+            logging.critical("Error: {0}".format(e))
+            return False
+
 
